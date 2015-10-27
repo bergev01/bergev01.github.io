fcae47d2900bfa902ee1a80fbdb84c9c8ec9fc7f
bergev01 <bergev01@luther.edu>
2015-10-27 15:43:27 -0500

    Update finaltodo.js

--- finaltodo.js
+++ finaltodo.js
@@ -1,14 +1,30 @@
-newTask=function() {
-  var todolist, box, newitem, taskstuff, addtask, newclass; 
-  todolist=document.querySelector("#todolist");
-  newitem=document.createElement("li");
-  newclass=document.querySelector("#priority").value;
-  newitem.className=newclass;
+window.onload=function() {
+  restorelist("todoul", doneTask);
+  }
+
+addTask=function() {
+  var todolist, box, newlist, taskstuff, addtask, newpriority; 
+  todolist=document.querySelector("#todoul");
+  newlist=document.createElement("li");
+  newpriority=document.querySelector("#priority").value;
+  newlist.className=newpriority;
   box=document.createElement("input");
   box.type="checkbox"; 
+  box.onclick=doneTask;
   taskstuff=document.querySelector("#taskbox").value;
   addtask=document.createTextNode(taskstuff);
-  newitem.appendChild(box);
-  newitem.appendChild(addtask);
-  todolist.appendChild(newitem);
+  newlist.appendChild(box);
+  newlist.appendChild(addtask);
+  todolist.appendChild(newlist);
+  localSave("todoul");
+}
+
+doneTask=function() {
+  if(this.checked){
+    this.parentNode.classList.add("done");
+    localSave("todoul");
+  } else {
+    this.parentNode.classList.remove("done");
+    localSave("todoul");
+  }
 }
